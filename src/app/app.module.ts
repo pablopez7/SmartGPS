cd3b4b5b5316024a38bb39f198bd81d3064334e6
Pablo López <pablopez7@gmail.com>
2018-11-18 19:04:08 -0600

    Estructura de Archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { BreadcrumbsComponent } from './components/shared/breadcrumbs/breadcrumbs.component';
+import { FloatingButtonComponent } from './components/shared/floating-button/floating-button.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { PagenotfoundComponent } from './components/auth/pagenotfound/pagenotfound.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/main/dashboard/dashboard.component';
+import { UsersComponent } from './components/admin/users/users.component';
+import { UserComponent } from './components/main/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AdminComponent,
+    AuthComponent,
+    BreadcrumbsComponent,
+    FloatingButtonComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidenavComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    PagenotfoundComponent,
+    RegisterComponent,
+    DashboardComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
